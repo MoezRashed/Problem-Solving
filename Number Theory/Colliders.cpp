244e66694f8a9a078f4a1f322057cbdf3aece20c
Moez <moezalaa@gmail.com>
2023-02-09 00:01:23 +0200

    uploading template

---
+++ Number Theory/Colliders.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ii pair<int,int>
+#define vi vector<int>
+#define vb vector<bool>
+#define pb push_back
+#define vii vector<ii>
+#define lli long long int
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ins insert
+#define mp make_pair
---
+++ Number Theory/PON - Prime or Not.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ii pair<int,int>
+#define vi vector<int>
+#define pb push_back
+#define vii vector<ii>
+#define lli long long int
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ins insert
+#define mp make_pair
+
+using namespace std;
+u64 binpower(u64 base, u64 e, u64 mod) {
+    u64 result = 1;
+    base %= mod;
+    while (e) {
+        if (e & 1)
+            result = (u128)result * base % mod;
+        base = (u128)base * base % mod;
+        e >>= 1;
+    }
+    return result;
+}
+bool probablyPrimeFermat(int n, int iter=20) {
+    if (n < 4)
+        return n == 2 || n == 3;
+
+    for (int i = 0; i < iter; i++) {
+        int a = 2 + rand() % (n - 3);
+        if (binpower(a, n - 1, n) != 1)
+            return false;
+    }
+    return true;
+}
